7b9e9206f464e768fa6a68e49f991b836de0df70
LostDiffusion <tassit@gmail.com>
2024-05-20 17:15:34 -0400

    Update main.py
    Add code from the safetensors tutorial.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #!/bin/python
+import torch
+from safetensors import safe_open
+from safetensors.torch import save_file
+
+def main():
+    print("[+] Starting... \n")
+
+    tensors = {
+        "weight1": torch.zeros((1024)),
+        "weight2": torch.zeros((1024))
+    }
+
+    # Save the file with the tensors we just created.
+    save_file(tensors, "model.safetensors")
+
+    # Clear the current tensor variable
+    tensors = {}
+
+    # Read in the tensor we saved
+    with safe_open("model.safetensors", framework="pt", device="cpu") as f:
+        for key in f.keys():
+            tensors[key] = f.get_tensor(key)
+        print("[+] Tensor read.")
+
+
+
+if __name__ == "__main__":
+    main()
